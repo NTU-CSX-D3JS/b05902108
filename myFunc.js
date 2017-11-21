7671d7aee26d7272afcd45e7afc5a8c3d3421416
strongshih <cs861324@gmail.com>
2017-11-21 05:13:01 -0600

    Update myFunc.js

--- myFunc.js
+++ myFunc.js
@@ -1,14 +1,18 @@
-function mean(arr){
+function sum(arr){
 	var ret = 0;
 	for(var i = 0; i < arr.length; i++)
 		ret = ret + parseInt(arr[i]);
 	return ret;
 }
+function mean(arr){
+	var s = sum(arr);
+	return s/arr.length;
+}
 function std_dev(arr){
 	var m = mean(arr);
-    var x = 0;
+    	var x = 0;
 	for(var i = 0; i < arr.length; i++)
 		x = x + (parseInt(arr[i])-m)*(parseInt(arr[i])-m);
-    x = Math.sqrt(x/m);
+    	x = Math.sqrt(x/m);
 	return x;
 }
